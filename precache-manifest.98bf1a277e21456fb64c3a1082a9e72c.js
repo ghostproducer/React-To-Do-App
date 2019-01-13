d5f7d833a8ea692c88f6ba59c41b002c391209aa
Matheus Zanetti <matheuszanetti92@gmail.com>
2019-01-13 17:48:54 -0200

    Updates

---
+++ precache-manifest.98bf1a277e21456fb64c3a1082a9e72c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39037ce6d62ef694ffd3",
+    "url": "/React-To-Do-App/static/js/runtime~main.39037ce6.js"
+  },
+  {
+    "revision": "91804d9e32f5071d466d",
+    "url": "/React-To-Do-App/static/js/main.91804d9e.chunk.js"
+  },
+  {
+    "revision": "ee9085498ae30fa60bcf",
+    "url": "/React-To-Do-App/static/js/1.ee908549.chunk.js"
+  },
+  {
+    "revision": "91804d9e32f5071d466d",
+    "url": "/React-To-Do-App/static/css/main.7fe85554.chunk.css"
+  },
+  {
+    "revision": "ecdb244b515cbcbe7ae2beafe74ab0c0",
+    "url": "/React-To-Do-App/index.html"
+  }
+];
